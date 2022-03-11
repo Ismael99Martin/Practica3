16e97a9f85087c55cb0c47930ad5fb0768d50527
ismae <'ismae@DESKTOP-40CVENT.com>
2022-03-11 20:02:36 +0100

    cuarto commit

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -5,6 +5,7 @@ public class Prueba {
 	public static void main(String[] args) {
 		System.out.println("Primera visualización");
 		System.out.println("Segunda visualización");
+		System.out.println("Tercera visualización");
 	}
 
 }
